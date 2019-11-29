7ccd5bd3db21bbea8607c8ceaebf5e779348d146
Noah Hellman <noah.hellman@protonmail.com>
2019-11-29 17:40:36 +0200

    add preprocessing functions: scale and center digits

---
+++ src/lib/preprocess.m
@@ -0,0 +1,19 @@
+% apply all preprocessing steps to a digit
+function d_new = preprocess(d, W)
+    d_new = normalize(d, W);
+end
+
+% scale and center digit to WxW square
+function d_new = normalize(d, W)
+    n = length(d);
+    dmin = min(d); dmax = max(d);
+    w = dmax(1)-dmin(1); h = dmax(2)-dmin(2);
+    scale = W/max(w, h);
+    x_start = (W-w*scale)/2;
+
+    d_new = zeros(n, 2);
+    for k = 1:n
+        d_new(k, 1) = x_start + (d(k, 1)-dmin(1))*scale;
+        d_new(k, 2) = (d(k, 2)-dmin(2))*scale;
+    end
+end
---
+++ src/lib/read_digits.m
@@ -0,0 +1,9 @@
+% read labeled digits located in dirpath
+function digits = read_digits(dirpath)
+    digits = cell(10, 1);
+    for i = 0:9
+        path = strcat(dirpath, '/stroke_', int2str(i), '_*.mat');
+        f = @(x) load(strcat(x.folder, "/", x.name));
+        digits{i+1} = struct2cell(arrayfun(f, dir(path)));
+    end
+end
