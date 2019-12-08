e0b88b27e680338eb6c24a83a2c056d8642a0e16
Noah Hellman <noah.hellman@protonmail.com>
2019-12-08 16:32:20 +0200

    add missing file

---
+++ src/mlp/backpropagate.m
@@ -0,0 +1,13 @@
+function [wOutput, wHidden] = ...
+  backpropagate(x, wHidden, wOutput, yHidden, yOutput, trainOutput)
+    rho = 0.0001;
+
+    deltaOutput = (yOutput-trainOutput);
+    deltaHidden = (wOutput(1:end-1, :)*deltaOutput) .* ...
+                  (1 - yHidden(1:end-1,:).^2);
+
+    deltawHidden = -rho*x'*deltaHidden';
+    deltawOutput = -rho*yHidden*deltaOutput';
+    wOutput = wOutput + deltawOutput;
+    wHidden = wHidden + deltawHidden;
+end
